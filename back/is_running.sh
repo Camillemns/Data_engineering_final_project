91d84bd18ade726d51bf3a0a704c9e282d15b808
Camille <camille.emma@gmail.com>
2022-02-28 17:41:13 +0100

    correcting is_running script

--- back/is_running.sh
+++ back/is_running.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-while :; do
-  curl -sS --fail -o /dev/null "http://localhost:8000/" && break
-  sleep 1
+while :
+do
+    curl -sS --fail -o /dev/null "http://localhost:8000/" && break
+    sleep 1
 done
