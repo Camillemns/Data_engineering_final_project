4740403afc0c6ff8bce1a2db16ead083ac3b08e9
Camille <camille.emma@gmail.com>
2022-03-03 23:01:34 +0100

    adding a counter and two gauges for the software metrics

--- back/app.py
+++ back/app.py
@@ -1,5 +1,7 @@
 from typing import Optional
+from urllib import request
 from fastapi import FastAPI
+import time
 from fastapi.middleware.cors import CORSMiddleware
 from model import prediction_toxicity, load_model,get_model_prediction
 from pydantic import BaseModel
@@ -20,6 +22,10 @@ items = {}
 
 start_http_server(8010)
 
+request_toxicity_counter = Counter('request_toxicity_counter', 'Number of sentence where the toxicity has been predicted.')
+in_progress_counter = Gauge('in_progress_counter', 'Number of predictions in progress.')
+last_prediction_time = Gauge('last_prediction_time', 'The last time a prediction was done.')
+
 class PredictionData(BaseModel):
     text: str
 
@@ -37,7 +43,11 @@ def read_root():
 
 @app.post("/get_toxicity/")
 async def prediction(data: PredictionData):
+    in_progress_counter.inc()
     p = get_model_prediction(items['model'], data.text)
+    last_prediction_time.set(time.time())
+    in_progress_counter.dec()
+    request_toxicity_counter.inc()
     for k, v in p.items():
         p[k] = float(v)
     return {"result": p}
